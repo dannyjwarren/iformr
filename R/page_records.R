06271760712ca19b58fa34f256d1ae53f1b22667
Are Strom <are.strom@gmail.com>
2017-07-06 17:20:39 -0700

    Added page records functions

---
+++ R/page_records.R
@@ -0,0 +1,219 @@
+#' Get list of all pages (forms, parent and subforms) in a profile
+#'
+#' Sends a request to the iFormBuilder API to get a listing of all
+#' forms and subforms in the current profile. Returns a dataframe
+#' with form id and form name.
+#'
+#' @rdname get_pages_list
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param profile_id The ID number of your profile
+#' @param limit The maximum number of form ids to return
+#' @param offset Skips the offset number of ids before beginning to return
+#' @param access_token The access_token required to establish communication with the API
+#' @return A dataframe of all forms (pages) in the given profile
+#' @export
+get_pages_list <- function(server_name,
+                           profile_id,
+                           limit = 1000,
+                           offset = 0,
+                           access_token) {
+  pages_uri <- paste0(api_v60_url(company = company_name),
+                      profile_id, "/pages?fields=fields&limit=",
+                      limit,
+                      "&offset=", offset)
+  bearer <- paste0("Bearer ", access_token)
+  r <- httr::GET(url = pages_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  pgs <- httr::content(r, type = "application/json")
+  pgsx <- integer(length(pgs))
+  pgs_id <- unlist(lapply(seq_along(pgsx),
+                          function(i) pgsx[i] <- pgs[[i]]$id))
+  pgs_name <- unlist(lapply(seq_along(pgsx),
+                            function(i) pgsx[i] <- pgs[[i]]$name))
+  data_frame(id = pgs_id, name = pgs_name)
+}
+
+#' Get id of a single page (form) given a form name
+#'
+#' Sends a request to the iFormBuilder API to get a table containing
+#' the id and name of a single form. You only need to supply the name
+#' of the option list. Returns a dataframe with form id and form name.
+#'
+#' @rdname get_page_id
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param profile_id The ID number of your profile
+#' @param page_name The name of the form or subform
+#' @param limit The maximum number of form ids to return
+#' @param offset Skips the offset number of ids before beginning to return
+#' @param access_token The access_token required to establish communication with the API
+#' @return A dataframe listing the id and name of the given form
+#' @export
+get_page_id <- function(server_name,
+                      profile_id,
+                      page_name,
+                      limit = 1000,
+                      offset = 0,
+                      access_token) {
+  pages_uri <- paste0(api_v60_url(server_name = server_name),
+                      profile_id,
+                      "/pages?fields=fields&limit=", limit,
+                      "&offset=", offset)
+  bearer <- paste0("Bearer ", access_token)
+  r <- httr::GET(url = pages_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  pgs <- httr::content(r, type = "application/json")
+  pgsx <- integer(length(pgs))
+  pgs_id <- unlist(lapply(seq_along(pgsx),
+                          function(i) pgsx[i] <- pgs[[i]]$id))
+  pgs_name <- unlist(lapply(seq_along(pgsx),
+                            function(i) pgsx[i] <- pgs[[i]]$name))
+  pg = data_frame(id = pgs_id, name = pgs_name)
+  pg$id[pg$name == page_name]
+}
+
+#' Get list of all record IDs in a page (form)
+#'
+#' Sends a request to the iFormBuilder API to get a list of all record
+#' ids in a given form or subform.
+#'
+#' @rdname get_page_record_list
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param profile_id The ID number of your profile
+#' @param page_name The name of the form or subform
+#' @param limit The maximum number of form ids to return
+#' @param offset Skips the offset number of ids before beginning to return
+#' @param access_token The access_token required to establish communication with the API
+#' @return A vector of record ids from the given form
+#' @export
+get_page_record_list <- function(server_name,
+                                 profile_id,
+                                 page_id,
+                                 limit = 1000,
+                                 offset = 0,
+                                 access_token) {
+  recordlist_uri <- paste0(api_v60_url(server_name = server_name),
+                           profile_id,
+                           "/pages/", page_id,
+                           "/records?fields=fields&limit=", limit,
+                           "&offset=", offset)
+  bearer <- paste0("Bearer ", access_token)
+  r <- httr::GET(url = recordlist_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  rcrd <- httr::content(r, type = "application/json")
+  rcrdx <- integer(length(rcrd))
+  unlist(lapply(seq_along(rcrdx),
+                function(i) rcrdx[i] <- rcrd[[i]]$id))
+}
+
+#' Get a single record from a page (form)
+#'
+#' Sends a request to the iFormBuilder API to get a list of all record
+#' ids in a given form or subform.
+#'
+#' @rdname get_page_record
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param profile_id The ID number of your profile
+#' @param page_id The id for the form
+#' @param record_id The id for the specific record to return
+#' @param access_token The access_token required to establish communication with the API
+#' @return Dataframe of a single record from the given form
+#' @export
+get_page_record <- function(server_name,
+                            profile_id,
+                            page_id,
+                            record_id,
+                            access_token) {
+  pagerecord_uri <- paste0(api_v60_url(server_name = server_name),
+                           profile_id,
+                           "/pages/", page_id,
+                           "/records/", record_id)
+  bearer <- paste0("Bearer ", access_token)
+  r <- httr::GET(url = pagerecord_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  rcrd <- httr::content(r, type = "application/json")
+  rcru <- unlist(lapply(rcrd,
+                        function(x) ifelse(is.null(x), as.character(NA), x)))
+  as.data.frame(t(rcru), stringsAsFactors = FALSE)
+}
+
+#' Get specific set of fields in a page (form)
+#'
+#' Sends a request to the iFormBuilder API to get a list of all record
+#' ids in a given form or subform.
+#'
+#' @rdname get_selected_page_records
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param profile_id The ID number of your profile
+#' @param page_id The id of the form or subform
+#' @param fields A set of data fields (columns) to return
+#' @param limit The maximum number of records to return
+#' @param offset Skips the offset number of records before beginning to return
+#' @param access_token The access_token required to establish communication with the API
+#' @return A dataframe of records from the specified fields
+#' @export
+get_selected_page_records <- function(server_name,
+                                      profile_id,
+                                      page_id,
+                                      fields = fields,
+                                      limit = 100,
+                                      offset = 0,
+                                      access_token) {
+  recordlist_uri <- paste0(api_v60_url(server_name = server_name),
+                           profile_id,
+                           "/pages/", page_id,
+                           "/records?fields=", fields,
+                           "&limit=", limit,
+                           "&offset=", offset)
+  bearer <- paste0("Bearer ", access_token)
+  r <- httr::GET(url = recordlist_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  rcrd <- httr::content(r, type = "application/json")
+  rcrd <- lapply(rcrd, rm_nulls)
+  rcrd <- as.data.frame(do.call(rbind, lapply(rcrd, rbind)))
+  rcrd <- as_data_frame(rcrd)
+  rcrd[] <- lapply(rcrd, unlist)
+  rcrd
+}
+
+#' Compose a url to get data via the data feed mechanism
+#'
+#' Creates a url with username and password embedded that can be used to
+#' download data using the data-feed mechanism instead of the API. In
+#' general, this should be avoided, as the API mechanisms are much
+#' safer. Returns a json file with all records since the specified
+#' SINCE_ID.
+#'
+#' @rdname get_selected_page_records
+#' @param server_name The server name as encoded in the url: `https//server_name.iformbuilder.com`
+#' @param parent_form_id The id of the parent form
+#' @param profile_id The ID number of your profile
+#' @param parent_form_name The name of the parent form
+#' @param since_id The record id indicating where to start downloading
+#' @param user_label The name given to the username in the .Renviron file
+#' @param pw_label Skips the offset number of records before beginning to return
+#' @return A .json file of records from the specified fields
+#' @export
+data_feed_url = function(server_name,
+                         parent_form_id,
+                         profile_id,
+                         parent_form_name,
+                         since_id,
+                         user_label,
+                         pw_label) {
+  paste0(base_url(server_name), "/exzact/dataScoringJSON.php?",
+         "PAGE_ID=", parent_form_id, "&",
+         "TABLE_NAME=_data", profile_id, "_", parent_form_name, "&",
+         "SINCE_ID=", since_id,"&",
+         "USERNAME=", iform_user(user_label), "&",
+         "PASSWORD=", iform_pw(pw_label))
+}
