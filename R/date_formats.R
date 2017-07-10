eb61d76667ed87ecb8f1725c12235c8f75c39874
Are Strom <are.strom@gmail.com>
2017-07-10 09:16:25 -0700

    Got rid of itext_time function. Updated DESCRIPTION.

--- R/date_formats.R
+++ R/date_formats.R
@@ -1,21 +1,20 @@
 #' @title Convert datetime values output by iFormBuilder
 #'
-#' @description Date and time values output from iFormBuilder using either the
-#'   API functions or the data feed function can be difficult to work with. The
-#'   output may look like: \code{"2014-11-13T15:04:00+00.00"} or: \code{Fri Apr
-#'   08 2016 08:20:02 GMT-0700 (PDT)}, depending on which function is used. The
-#'   \code{idate_time(), and itext_time()} functions convert these datetime
-#'   outputs to standard format \code{"2014-10-13 08:04:03"}, taking timezone
-#'   into consideration.
+#' @description Date and time values output from iFormBuilder can be difficult
+#'   to work with. The output will typically look like:
+#'   \code{"2014-11-13T15:04:00+00.00"}. The \code{idate_time()} function
+#'   converts datetime outputs to standard format \code{"2014-10-13 08:04:03"}
+#'   taking timezone into consideration.
 #'
 #' @rdname idate_time
 #' @param dts A vector of datetimes as string values
 #' @param timezone A character string in standard format specifying timezone.
 #'   Use \code{OlsonNames()} for a list of standard time zone character strings.
-#' @seealso \code{\link{Sys.timezone}}
+#' @seealso \code{\link{Sys.timezone}} for timezones
 #' @examples
 #' # Create vector that straddles change in daylight savings time
-#' dts = c("2014-10-13T15:04:03+00:00", "", NA, "2014-11-13T15:04:03+00:00")
+#' # Daylight savings time occurred: 2017-03-12 02:00:00
+#' dts = c("2017-03-12T01:10:00+00:00", "", NA, "2017-03-12T02:10:00+00:00")
 #'
 #' # Convert to standard format
 #' idate_time(dts)
@@ -39,45 +38,3 @@ idate_time <- function (dts, timezone = "America/Los_Angeles") {
   gmt
 }
 
-#' @title Convert datetime values output by iFormBuilder
-#'
-#' @description Date and time values output from iFormBuilder using either the
-#'   API functions or the data feed function can be difficult to work with. The
-#'   output may look like: \code{"2014-11-13T15:04:00+00.00"} or: \code{Fri Apr
-#'   08 2016 08:20:02 GMT-0700 (PDT)}, depending on which function is used. The
-#'   \code{idate_time(), and itext_time()} functions convert these datetime
-#'   outputs to standard format \code{"2014-10-13 08:04:03"}, taking timezone
-#'   into consideration.
-#'
-#' @rdname itext_time
-#' @param dts A vector of datetimes as string values
-#' @param timezone A character string in standard format specifying timezone.
-#'   Use \code{OlsonNames()} for a list of standard time zone character strings.
-#' @param create_tz A character string in standard format specifying timezone.
-#' @seealso \code{\link{Sys.timezone}}
-#' @examples
-#' # Create vector that straddles change in daylight savings time
-#' dts = c("", NA, "Fri Apr 08 2016 08:20:02 GMT-0700 (PDT)",
-#'         "Fri Feb 08 2016 08:20:02 GMT-0700 (PST)")
-#'
-#' # Convert to standard format
-#' itext_time(dts)
-#' @export
-itext_time <- function (dts, create_tz = "America/Los_Angeles", timezone = "America/Los_Angeles") {
-  gmt = character(length(dts))
-  for( i in 1:length(dts)) {
-    if (is.na(dts[i]) | dts[i] == "") {
-      gmt[i] = as.character(NA)
-    } else if (nchar(dts[i]) > 25 & stringi::stri_detect_fixed(dts[i], "GMT") == TRUE) {
-      # Create gmt time value
-      gdt <-  format(as.Date(substr(dts[i], 5, 15), format = "%b %d %Y"))
-      gmt[i] <- paste0(gdt, " ", substr(dts[i], 17, 24))
-      pmt <-  as.POSIXct(gmt[i], tz = create_tz)
-      pmt <-  lubridate::with_tz(pmt, timezone)
-      gmt[i] <-  format(pmt, format = "%Y-%m-%d %H:%M:%S")
-    } else {
-      stop("Date or time values are in an unexpected format")
-    }
-  }
-  gmt
-}
