5a672deef501f7088dd3dba370d6a13d9ffc2437
DeVoe <William.DeVoe@maine.gov>
2018-09-24 11:59:56 -0400

    Added get_photos function and PDF of function documentation.

--- R/helper_functions.R
+++ R/helper_functions.R
@@ -9,7 +9,7 @@
 #' in a parent-to-child fashion:
 #' - Rows in the source data not in the form data are added (based on
 #' the unique identifier from the \code{uid} argument.)
-#' - Rows in the form data that are not prseent in the source data
+#' - Rows in the form data that are not present in the source data
 #' can be optionally removed using the \code{delete} argument.
 #' - Rows in the form data are updated if any of their fields differ
 #' from the matching row in the source data. Updating can be disabled
@@ -97,6 +97,10 @@ sync_table <- function(server_name, profile_id, access_token,
   {stop(paste0("UID column ",uid," is missing from source data."))}
   if (!(uid %in% ifb_flds))
   {stop(paste0("UID column ",uid," is missing from IFB data."))}
+  # Check that UID is unique and non-null in incoming data
+  if (any(is.na(data[[uid]]))) {stop(paste0("Input data has NA values in UID column ",uid))}
+  if ("" %in% data[[uid]]) {stop(paste0("Input data has blank values in UID column ",uid))}
+  if (any(duplicated(data[[uid]]))) {stop(paste0("Input data has duplicate values in UID column ",uid))}
   # Pull all data in IFB table
   i_data <- get_all_records(server_name, profile_id, page_id, fields = "fields",
                                     limit = 1000, offset = 0, access_token,
@@ -354,4 +358,63 @@ format_name <- function(name) {
   return(name)
 }
 
-
+#' @title Get photos from form
+#'
+#' @description Downloads all of the photos taken with the photo element in a form
+#' to a local directory, naming the files based on another field in the form.
+#' Since the filenames produced in IFB are jibberish to people, the recommended practice
+#' is to include a dynamically calculated element to uniquely identify each photo.
+#' For example, in a workflow with a trip form, station subform, and station_photos
+#' subform, the station_photos form would include a photoid field that created unique
+#' photoids by concaternation of the trip ID, site ID, and index of the photo record.
+#'
+#' @rdname get_photos
+#' @author Bill Devoe, \email{William.DeVoe@@maine.gov}
+#' @param server_name String of the iFormBuilder server name.
+#' @param profile_id Integer of the iFormBuilder profile ID.
+#' @param access_token Access token produced by \code{iformr::get_iform_access_token}
+#' @param page_id Integer ID of the form to download photos from.
+#' @param photo Character string of the photo element's DCN.
+#' @param output Path of the output directory for the downloaded photos.
+#' @param photoid Character string of the DCN to use as a filename
+#' for the photo. Must contain unique values.
+#' @return Boolean True if succesful.
+#' @examples
+#' \dontrun{
+#' # Get access_token
+#' access_token <- get_iform_access_token(
+#'   server_name = "your_server_name",
+#'   client_key_name = "your_client_key_name",
+#'   client_secret_name = "your_client_secret_name")
+#'
+#' # Download photos from a form
+#'
+#' }
+#' @export
+get_photos <- function(server_name, profile_id, access_token,
+                          page_id, photo, output, photoid) {
+  # Make a list of fields to get from the form
+  flds <- c(photo, photoid)
+  fldstr <- paste(flds, collapse = ',')
+  # Get all the data from the page
+  data <- get_all_records(server_name, profile_id, page_id,
+                  access_token = access_token, field_string = fldstr, since_id = 0)
+  # Check that photo and photoid columns exist
+  if (!(photo %in% colnames(data))) {stop(paste0("Form data is missing photo element ",photo))}
+  if (!(photoid %in% colnames(data))) {stop(paste0("Form data is missing photo id ",photoid))}
+  # Check that photoids are unique
+  if (any(is.na(data[[photoid]]))) {stop(paste0("Form data has NA values in photoid field ",photoid))}
+  if ("" %in% data[[photoid]]) {stop(paste0("Form data has blank values in photoid field ",photoid))}
+  if (any(duplicated(data[photoid]))) {stop(paste0("Form data has non-unique values in photoid field ",photoid))}
+  # Check that output path exists, and if not create it
+  if (!(dir.exists(output))) {dir.create(file.path(output))}
+  # Download each photo from the photo DCN and name it with the photoid
+  for (row in 1:nrow(data)) {
+    message(row)
+    url <- data[[photo]][row]
+    filename <- paste0(data[[photoid]][row],'.jpg')
+    path <- file.path(output, filename)
+    try(download.file(url, path, quiet = FALSE, mode = "wb"))
+  }
+  return(T)
+}
