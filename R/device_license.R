a31d009b3192495979299a0c7ef2b2d87562a9db
Are Strom <are.strom@gmail.com>
2020-04-07 06:53:30 -0700

    Initial commit

---
+++ R/device_license.R
@@ -0,0 +1,86 @@
+#'
+#' Retrieve a list of device licenses
+#'
+#' Retrieves a list of all device licenses in a profile. Core API call [Retrieve a List of Device Licenses](
+#' https://iformbuilder.docs.apiary.io/#reference/device-license-resource/device-license-collection/retrieve-a-list-of-device-licenses)
+#'
+#' @rdname retrieve_all_device_licenses
+#' @param server_name String of the iFormBuilder server name.
+#' @param profile_id Integer of the iFormBuilder profile ID.
+#' @param access_token Access token produced by \code{\link{get_iform_access_token}}
+#' @return List containing the ID, username, and device_id for all device licenses in a profile
+#' @import httr
+#'
+#' @examples
+#' \dontrun{
+#' # Get access_token
+#' access_token <- get_iform_access_token(
+#'   server_name = "your_server_name",
+#'   client_key_name = "your_client_key_name",
+#'   client_secret_name = "your_client_secret_name")
+#'
+#' # Retrieve a list of all device licenses in a profile
+#' all_device_license_info <- retrieve_all_device_licenses(
+#'   server_name = "wdfw",
+#'   profile_id = 123456,
+#'   access_token = access_token)
+
+#' # Pull out the first device license for next function
+#' license_id = all_device_license_info[[1]]$id
+#'
+#' @export
+retrieve_all_device_licenses <- function(server_name, profile_id, access_token) {
+  # Build URL
+  all_licenses_uri <- paste0(api_v60_url(server_name = server_name),
+                             profile_id, "/licenses")
+  bearer <- paste0("Bearer ", access_token)
+  # Build request
+  r <- httr::GET(url = all_licenses_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  all_device_licenses <- httr::content(r, type = "application/json")
+  return(all_device_licenses)
+}
+
+#' Retrieve a device license
+#'
+#' Retrieves a single device license. Core API call [Retrieve a Device License](
+#' https://iformbuilder.docs.apiary.io/#reference/device-license-resource/device-license/retrieve-a-device-license)
+#'
+#' @rdname retrieve_device_license
+#' @author Bill Devoe, \email{William.DeVoe@@maine.gov}
+#' @param server_name String of the iFormBuilder server name.
+#' @param profile_id Integer of the iFormBuilder profile ID.
+#' @param license_id Integer id of the device license.
+#' @param access_token Access token produced by \code{\link{get_iform_access_token}}
+#' @return List containing attributes of the device license
+#' @import httr
+#'
+#' @examples
+#' \dontrun{
+#' # Retrieve details for a single device license
+#' single_device_license_info <- retrieve_device_license(
+#'   server_name = "wdfw",
+#'   profile_id = 123456,
+#'   license_id = license_id,
+#'   access_token = access_token)
+
+#' # Pull out the first device license for next function
+#' license_id = all_device_license_info[[1]]$id
+#'
+#'#' @export
+retrieve_device_license <- function(server_name, profile_id,
+                                    access_token, license_id) {
+  # Build URL
+  license_uri <- paste0(api_v60_url(server_name = server_name),
+                        profile_id, "/licenses/", license_id)
+  bearer <- paste0("Bearer ", access_token)
+  # Build request
+  r <- httr::GET(url = license_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  device_license <- httr::content(r, type = "application/json")
+  return(device_license)
+}
---
+++ R/users.R
@@ -0,0 +1,80 @@
+#'
+#' Retrieve a list of all users in a profile
+#'
+#' Retrieves a list of all users in a profile. Core API call [Retrieve a List of Users](
+#' https://iformbuilder.docs.apiary.io/#reference/user-resource/user-collection/retrieve-a-list-of-users)
+#'
+#' @rdname retrieve_all_users
+#' @param server_name String of the iFormBuilder server name.
+#' @param profile_id Integer of the iFormBuilder profile ID.
+#' @param access_token Access token produced by \code{\link{get_iform_access_token}}
+#' @return List containing all usernames and user IDs in the profile.
+#'
+#' @examples
+#' \dontrun{
+#' # Get access_token
+#' access_token <- get_iform_access_token(
+#'   server_name = "your_server_name",
+#'   client_key_name = "your_client_key_name",
+#'   client_secret_name = "your_client_secret_name")
+#'
+#' # Get a list of all usernames and user IDs in a profile
+#' all_users_info <- retrieve_all_users(
+#'   server_name = "your_server_name",
+#'   profile_id = 123456,
+#'   access_token = access_token)
+#'
+# Pull out first user_id in the list
+#' user_id = all_users_info[[1]]$id
+#'
+#' @export
+retrieve_all_users <- function(server_name, profile_id, access_token) {
+  # Build URL
+  retrieve_all_users_uri <- paste0(api_v60_url(server_name = server_name),
+                               profile_id, "/users")
+  bearer <- paste0("Bearer ", access_token)
+  # Build request
+  r <- httr::GET(url = retrieve_all_users_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  user_ids = httr::content(r, type = "application/json")
+  return(user_ids)
+}
+
+#'
+#' Retrieve a user
+#'
+#' Retrieves information for a specific user. Core API call [Retrieve a User](
+#' https://iformbuilder.docs.apiary.io/#reference/user-resource/user/retrieve-a-user)
+#'
+#' @rdname retrieve_user
+#' @author Bill Devoe, \email{William.DeVoe@@maine.gov}
+#' @param server_name String of the iFormBuilder server name.
+#' @param profile_id Integer of the iFormBuilder profile ID.
+#' @param user_id Integer of the user ID to retrieve.
+#' @param access_token Access token produced by \code{\link{get_iform_access_token}}
+#' @return List containing the user details.
+#'
+#' #' # Get a list of details for a given user
+#' all_users_info <- retrieve_all_users(
+#'   server_name = "your_server_name",
+#'   profile_id = 123456,
+#'   user_id = user_id,
+#'   access_token = access_token)
+#'
+#' @export
+retrieve_user <- function(server_name, profile_id,
+                          user_id, access_token) {
+  # Build URL
+  retrieve_user_uri <- paste0(api_v60_url(server_name = server_name),
+                              profile_id, "/users/", user_id)
+  bearer <- paste0("Bearer ", access_token)
+  # Build request
+  r <- httr::GET(url = retrieve_user_uri,
+                 httr::add_headers('Authorization' = bearer),
+                 encode = "json")
+  httr::stop_for_status(r)
+  user_ids = httr::content(r, type = "application/json")
+  return(user_ids)
+}
